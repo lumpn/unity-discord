159d3d2201e6d002fe76ee52f5a28f3b8fdf6903
lumpn <lumpn@users.noreply.github.com>
2024-10-11 13:40:31 +0800

    Support creating multipart web requests.

--- Runtime/Webhook.cs
+++ Runtime/Webhook.cs
@@ -3,6 +3,9 @@
 // Copyright(c) 2021 Jonas Boetel
 //----------------------------------------
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Text;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -32,6 +35,40 @@ namespace Lumpn.Discord
 
             var request = new UnityWebRequest(uri, UnityWebRequest.kHttpVerbPOST, downloadHandler, uploadHandler);
             return request;
+        }
+
+        public UnityWebRequest CreateWebRequest(Message message, byte[] attachmentBytes)
+        {
+            var sections = new List<IMultipartFormSection>
+            {
+                CreateDataSection(message),
+                CreateFileSection(attachmentBytes,0),
+            };
+
+            return UnityWebRequest.Post(uri, sections);
+        }
+
+        public UnityWebRequest CreateWebRequest(Message message, IEnumerable<byte[]> multipleAttachmentBytes)
+        {
+            var sections = new List<IMultipartFormSection>
+            {
+                CreateDataSection(message),
+            };
+            sections.AddRange(multipleAttachmentBytes.Select(CreateFileSection));
+
+            return UnityWebRequest.Post(uri, sections);
+        }
+
+        private static MultipartFormDataSection CreateDataSection(Message message)
+        {
+            var json = JsonUtility.ToJson(message);
+            return new MultipartFormDataSection("payload_json", json, Encoding.UTF8, "application/json");
+        }
+
+        private static MultipartFormFileSection CreateFileSection(byte[] bytes, int index)
+        {
+            var name = string.Format(CultureInfo.InvariantCulture, "files[{0}]", index);
+            return new MultipartFormFileSection(name, bytes, null, "application/octet-stream");
         }
     }
 }
